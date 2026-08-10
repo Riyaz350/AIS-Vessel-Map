91f858ffc94c952fcf6e2dbb135d67728a3c8849
unknown <riyaz3582@gmail.com>
2026-08-10 22:45:39 +0600

    feat: wire AI command bar to vessel map via ref

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,27 @@
+import { useRef } from 'react'; 
+
 import VesselMap from './components/VesselMap'; 
+
+import AICommandBar from './components/AICommandBar'; 
 
   
 
 export default function App() { 
 
-  return <VesselMap />; 
+  const vesselMapRef = useRef(null); 
+
+  
+
+  return ( 
+
+    <> 
+
+      <VesselMap ref={vesselMapRef} /> 
+
+      <AICommandBar vesselMapRef={vesselMapRef} /> 
+
+    </> 
+
+  ); 
 
 } 
