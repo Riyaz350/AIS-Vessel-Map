02f45af50d17737d73be119b06b9e3986b62fcf0
S.K. Riyaz Ahmed <skriyazahmed200@gmail.com>
2026-09-14 23:43:47 +0600

    added session replay

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,15 +8,16 @@ import App from './App';
 Sentry.init({
   dsn: 'https://5a827c26800cc629374653cc345f9f14@o4512084899921920.ingest.us.sentry.io/4512084910473216',
 
-  integrations: [
-    Sentry.replayIntegration(),
-  ],
+
 
   // Record 10% of normal user sessions
   replaysSessionSampleRate: 0.1,
 
   // Record 100% of sessions where an error occurs
   replaysOnErrorSampleRate: 1.0,
+  integrations: [
+    Sentry.replayIntegration(),
+  ],
 });
 
 ReactDOM.createRoot(document.getElementById('root')).render(
